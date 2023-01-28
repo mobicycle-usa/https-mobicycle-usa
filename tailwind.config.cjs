6395da6d370fb422b994a3a9d60f91498c9ad163
MobiCycle <admin@mobicycle.eu>
2023-01-28 08:16:36 -0800

    updates

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+	  extend: {
+		colors: {
+		  purple: {
+			650: "#4a5899",
+		  },
+		  orange: {
+			650: "#dfab82",
+		  },
+		  yellow: {
+			650: "#fff4d3",
+		  },
+		},
+	  },
+	},
+	plugins: [],
+  };
+  
