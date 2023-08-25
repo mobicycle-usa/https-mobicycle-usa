d91d0dd6b47dda98c059c1a74091c2f908124929
MobiCycle <admin@mobicycle.consulting>
2023-08-25 00:31:28 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), sitemap()]
 });
