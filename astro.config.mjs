814ed4150cdfcc26686ab46d0273e79d52766ae5
mobicycle-us <rose@mobicycle.us>
2024-06-17 15:57:00 -0700

    revamped content to more closely match other sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,9 @@ import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://https-mobicycle-usa.pages.dev/",
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({platformProxy: {enabled: true,},}),
   integrations: [tailwind(), sitemap()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-	  extend: {
+		extend: {
+			backgroundImage: {
+				'picture1': 'url(/cdn-cgi/image/width=2000,quality=80/https://imagedelivery.net/uIknt2TlDR57WZHMJo_oWQ/00c06c7c-f42b-49e5-648d-3c7983e55400/original)',
+				'picture2': 'url(/cdn-cgi/image/width=1080,quality=80/https://imagedelivery.net/uIknt2TlDR57WZHMJo_oWQ/42b49db8-3006-48a3-ebe7-c184a596b500/original)',
+			  }
+      },
 		colors: {
 		  purple: {
 			650: "#4a5899",
@@ -12,6 +17,13 @@ module.exports = {
 		  },
 		  yellow: {
 			650: "#fff4d3",
+		  },
+		  white: {
+			650: "#ffffff",
+			opacity: 50,
+		  },
+		  slate: {
+			650: "#111827",
 		  },
 		},
 		leading: {
@@ -29,9 +41,4 @@ module.exports = {
 		'display': ['Oswald'],
 		'body': ['"Open Sans"'],
 	  }
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('@tailwindcss/forms'),
-	],
-  };
+	}
